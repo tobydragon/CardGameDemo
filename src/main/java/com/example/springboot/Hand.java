4e4a87c463f45b8984cef00d8a4d185714e044cc
Stephen <stevoschool@gmail.com>
2020-06-04 15:23:57 -0400

    added implementation for the Hand constructor

--- src/main/java/com/example/springboot/Hand.java
+++ src/main/java/com/example/springboot/Hand.java
@@ -1,12 +1,23 @@
 package com.example.springboot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Hand {
     private ArrayList<Card> cards;
 
-    public Hand(ArrayList<Card> startingCards) throws IllegalArgumentException{}
-    public Hand(){}
+    public Hand(ArrayList<Card> startingCards) throws IllegalArgumentException{
+        cards = new ArrayList<>();
+        Collections.sort(startingCards);
+        for(int x = 0; x < startingCards.size() -1; x ++){
+            if(startingCards.get(x).compareTo(startingCards.get(x + 1)) == 0)
+                throw new IllegalArgumentException("Cannot have duplicate cards in hand");
+        }
+        cards = (ArrayList<Card>)startingCards.clone();
+    }
+    public Hand(){
+        cards = new ArrayList<>();
+    }
 
     public ArrayList<Card> getCards() {
         return cards;
