973486dcb8d0de3a28bd81ca54aaf6f57244c3f6
Stephen <stevoschool@gmail.com>
2020-06-04 14:32:49 -0400

    moved class tests to seperate package

--- src/test/java/com/example/classes/DeckTest.java
+++ src/test/java/com/example/classes/DeckTest.java
@@ -1,5 +1,7 @@
-package com.example.springboot;
+package com.example.classes;
 
+import com.example.springboot.Card;
+import com.example.springboot.Deck;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
