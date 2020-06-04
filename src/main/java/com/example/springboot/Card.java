37edc69efaa17023b5cef2172f66c897b264023b
Stephen <stevoschool@gmail.com>
2020-06-04 15:16:21 -0400

    added implementation for Card.comparable

--- src/main/java/com/example/springboot/Card.java
+++ src/main/java/com/example/springboot/Card.java
@@ -1,6 +1,27 @@
 package com.example.springboot;
 
-public class Card {
+public class Card implements Comparable{
+    @Override
+    public int compareTo(Object o) {
+        Card rhs = (Card)o;
+        if(this.value > rhs.value)
+            return -1;
+        else if(this.value < rhs.value)
+            return 1;
+        else if(this.suit == rhs.suit)
+            return 0;
+        else{
+            int lI = -1, rI = -1;
+            for(int x = 0; x < 4; x ++){
+                if(this.suit == Suit.values()[x])
+                    lI = x;
+                if(rhs.suit == Suit.values()[x])
+                    rI = x;
+            }
+            return lI > rI ? 1:-1;
+        }
+    }
+
     public enum Suit{
         SPADE,HEART,DIAMOND,CLUB
     }
