d4d5446d9a10c8d3267279d15b2b5d5e8f3b95d3
Stephen <stevoschool@gmail.com>
2020-06-04 14:54:17 -0400

    added tests for Hand

--- src/main/java/com/example/springboot/Hand.java
+++ src/main/java/com/example/springboot/Hand.java
@@ -5,5 +5,10 @@ import java.util.ArrayList;
 public class Hand {
     private ArrayList<Card> cards;
 
+    public Hand(ArrayList<Card> startingCards) throws IllegalArgumentException{}
+    public Hand(){}
 
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
 }
---
+++ src/test/java/com/example/classes/HandTest.java
@@ -0,0 +1,41 @@
+package com.example.classes;
+
+
+import com.example.springboot.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HandTest {
+
+    @Test
+    public void ConstructorTest(){
+        Hand h1 = new Hand();
+        assertEquals(0, h1.getCards().size());
+
+        ArrayList<Card> c1 = new ArrayList<>();
+        c1.add(new Card(Card.Suit.CLUB, 1));
+        c1.add(new Card(Card.Suit.DIAMOND, 1));
+        c1.add(new Card(Card.Suit.HEART, 1));
+        c1.add(new Card(Card.Suit.CLUB, 13));
+        c1.add(new Card(Card.Suit.SPADE, 2));
+
+        h1 = new Hand(c1);
+        assertEquals(5, h1.getCards().size());
+        assertArrayEquals(c1.toArray(), h1.getCards().toArray());
+
+        c1.add(new Card(Card.Suit.CLUB, 1));
+        try{
+            Hand h2 = new Hand(c1);
+            fail();
+        }
+        catch(IllegalArgumentException e){
+            assertEquals("Cannot have duplicate cards in hand", e.getMessage());
+        }
+    }
+
+
+
+}
