1c30e779b4924b011c6c92e4adeeadb4b830f748
Stephen <stevoschool@gmail.com>
2020-06-05 13:37:49 -0400

    Tests for getHand mapping

--- src/main/java/edu/ithaca/dragon/spring/BlackJackController.java
+++ src/main/java/edu/ithaca/dragon/spring/BlackJackController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,11 +16,24 @@ public class BlackJackController {
 
     public BlackJackController(){
         games = new HashMap<>();
+        Player p1 = new Player(0, new Hand());
+        p1.addCardToHand(0, new Card(Card.Suit.SPADE, 1));
+        p1.addCardToHand(0, new Card(Card.Suit.SPADE, 13));
+        BlackJack b1 = new BlackJack(0, p1);
+        games.put(b1.getID(), b1);
+        Player p2 = new Player(0, new Hand());
+        p2.addCardToHand(0, new Card(Card.Suit.DIAMOND, 10));
+        p2.addCardToHand(0, new Card(Card.Suit.DIAMOND, 12));
+        BlackJack b2 = new BlackJack(2, p2);
+        games.put(b2.getID(), b2);
     }
 
     @GetMapping("/api/blackjack/{id}")
     public Hand getHand(@PathVariable Long id){
+        ArrayList<Card> c1 = new ArrayList<>();
+        c1.add(new Card(Card.Suit.SPADE, 2));
         return null;
+        //return new Hand(c1);
     }
 
 
---
+++ src/test/java/edu/ithaca/dragon/spring/BlackJackControllerTest.java
@@ -0,0 +1,45 @@
+package edu.ithaca.dragon.spring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ithaca.dragon.blackjack.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BlackJackController.class)
+public class BlackJackControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getHandTest() throws Exception{
+        ArrayList<Card> c1 = new ArrayList<>();
+        c1.add(new Card(Card.Suit.SPADE, 1));
+        c1.add(new Card(Card.Suit.SPADE, 13));
+        Hand h1 = new Hand(c1);
+        ArrayList<Card> c2 = new ArrayList<>();
+        c2.add(new Card(Card.Suit.DIAMOND, 10));
+        c2.add(new Card(Card.Suit.DIAMOND, 12));
+        Hand h2 = new Hand(c2);
+        ObjectMapper mapper = new ObjectMapper();
+        String test = mapper.writeValueAsString(h1);
+        String test2 = mapper.writeValueAsString(h2);
+        this.mockMvc.perform(get("/api/blackjack/0")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(equalTo(test)));
+        this.mockMvc.perform(get("/api/blackjack/1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("")));
+        this.mockMvc.perform(get("/api/blackjack/2")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(equalTo(test2)));
+    }
+}
