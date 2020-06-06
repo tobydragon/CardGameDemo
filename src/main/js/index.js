b8a74d2e5267810e5f8b0034a2fed7302858fb31
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-06 06:41:05 -0400

    added simple blackjack code in separate js file

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BlackJackGame from "./BlackJackGame";
 
 
 const reactElement = document.getElementById("react");
 ReactDOM.render(
     <React.StrictMode>
-        Hello from React file!
+        <BlackJackGame/>
     </React.StrictMode>,
     reactElement
 );
