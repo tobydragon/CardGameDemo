7bf47749d4bca6dd7856a066c2399e2a57d176e3
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-09 14:28:56 -0400

    fixed typo in index.js

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BlackJack from "./BlackJack";
+import BlackJackGame from "./BlackJack";
 
 
 const reactElement = document.getElementById("react");
 ReactDOM.render(
     <React.StrictMode>
-        <BlackJack/>
+        <BlackJackGame/>
     </React.StrictMode>,
     reactElement
 );
