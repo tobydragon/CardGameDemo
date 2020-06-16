9b9e2c308e06d0ae847cc7e22d1590077e6e21ca
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-16 15:56:34 -0400

    Added some better tests for equivalence classes

--- src/test/js/Hand.test.js
+++ src/test/js/Hand.test.js
@@ -5,17 +5,39 @@ import React from "react";
 import Hand from "../../main/js/Hand"
 import Card from "react-bootstrap/Card";
 
-const cards= [
+const regHandEx=[
+    { suit: "diamonds", value: 6 },
+    { suit: "hearts", value: 4 },
+    { suit: "clubs", value: 8},
+]
+const bigHandEx= [
     { suit: "spades", value: 2 },
     { suit: "hearts", value: 4 },
-    { suit: "diamonds", value: 6 },
-    { suit: "hearts", value: 5 }
+    { suit: "diamonds", value: 2 },
+    { suit: "hearts", value: 5 },
+    { suit: "hearts", value: 3 },
+    { suit: "clubs", value: 2 },
+    { suit: "hearts", value: 2 },
 ]
+const singleCardEx= [
+    { suit: "spades", value: 2 },
+]
+
 describe('Hand', () => {
-    it('check hand for right number of cards', () => {
-        const handResult = shallow(<Hand cards={cards}/>);
+    it('check hand different Hands', () => {
+        let handResult = shallow(<Hand cards={regHandEx}/>);
         // console.log(handResult.debug());
         // console.log(handResult.find(Card).debug());
-        expect(handResult.find(Card)).to.have.lengthOf(4);
+        expect(handResult.find(Card)).to.have.lengthOf(3);
+
+        handResult = shallow(<Hand cards={bigHandEx}/>);
+        expect(handResult.find(Card)).to.have.lengthOf(7);
+
+        handResult = shallow(<Hand cards={singleCardEx}/>);
+        expect(handResult.find(Card)).to.have.lengthOf(1);
+
+        handResult = shallow(<Hand cards={[]} />);
+        expect(handResult.find(Card)).to.have.lengthOf(0);
+
     });
 });
