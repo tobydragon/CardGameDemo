7366552ef73e9b6f58b00ca061ba5a9c22b5e780
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-16 15:42:59 -0400

    fixed enzyme testing to work for Hand

--- src/test/js/Hand.test.js
+++ src/test/js/Hand.test.js
@@ -1,7 +1,9 @@
+import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import React from "react";
 
 import Hand from "../../main/js/Hand"
+import Card from "react-bootstrap/Card";
 
 const cards= [
     { suit: "spades", value: 2 },
@@ -9,8 +11,11 @@ const cards= [
     { suit: "diamonds", value: 6 },
     { suit: "hearts", value: 5 }
 ]
-describe('First React component test with Enzyme', () => {
-    it('renders without crashing', () => {
-        shallow(<Hand cards={cards}/>);
+describe('Hand', () => {
+    it('check hand for right number of cards', () => {
+        const handResult = shallow(<Hand cards={cards}/>);
+        // console.log(handResult.debug());
+        // console.log(handResult.find(Card).debug());
+        expect(handResult.find(Card)).to.have.lengthOf(4);
     });
 });
