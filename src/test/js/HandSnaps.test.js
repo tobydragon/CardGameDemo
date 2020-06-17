0224bdd44cadc8cf2336d46b5a53a46151246676
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-17 09:01:49 -0400

    added snaps for all example hands

--- src/test/js/HandSnaps.test.js
+++ src/test/js/HandSnaps.test.js
@@ -2,10 +2,28 @@ import renderer from "react-test-renderer";
 import React from "react";
 
 import Hand from "../../main/js/Hand";
-import {regHandEx} from "./Hand.examples";
+import {regHandEx, bigHandEx, singleCardHandEx} from "./Hand.examples";
 
-test('hand snapshots test', () => {
+test('regHandEx snapshots test', () => {
     const component = renderer.create(<Hand cards={regHandEx}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('bigHandEx snapshots test', () => {
+    const component = renderer.create(<Hand cards={bigHandEx}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('singleCardHandEx snapshots test', () => {
+    const component = renderer.create(<Hand cards={singleCardHandEx}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('noCardHandEx snapshots test', () => {
+    const component = renderer.create(<Hand cards={[]}/>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
