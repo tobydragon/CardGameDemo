e8d818a26e44f87a9f35d107b4a10f98d6ca9cf4
Toby Dragon <dr.toby.dragon@gmail.com>
2020-06-19 08:46:32 -0400

    Added log of roundStateResponses in console

--- src/main/js/Round.js
+++ src/main/js/Round.js
@@ -30,6 +30,8 @@ export default class Round extends React.Component {
     }
 
     handleRoundStateResponse(responseJson){
+        console.log("INFO:\t Round.handleRoundStateResponse, got:");
+        console.log(responseJson);
         this.setState({
             playerCards: responseJson.playerHand.cards,
             dealerCards: responseJson.dealerHand.cards
